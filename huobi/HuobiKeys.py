5d7dbc47aab977238469b4ae7255a08cdcc33566
Erik Lee <li.jun4454@gmail.com>
2018-06-30 21:57:18 +0800

    huobi api utils and demo for getAccount

---
+++ huobi/HuobiKeys.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+HUOBI_ACCESS_KEY = ""
+HUOBO_SECRET_KEY = ""
---
+++ huobi/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from HuobiServices import *
+print("hello---------------")
+print(get_accounts())
+# print(get_balance())
