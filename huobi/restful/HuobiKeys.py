91ee7d558edf266e7a03ddee171656696ed78b83
Erik Lee <li.jun4454@gmail.com>
2018-07-08 17:43:58 +0800

    add huobi key file

---
+++ huobi/restful/HuobiKeys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# EXAMPLE:
+# HUOBI_ACCESS_KEY = "7bd2c78c-3c2d19b5-4854a9e7-dbgb4"
+# HUOBO_SECRET_KEY = "df715c3e-0e0eb7g7-054dh117-1e27a"
+HUOBI_ACCESS_KEY = ""
+HUOBO_SECRET_KEY = ""
