b82df3666c228b24842bea2404ddc6c7caa6f148
lionheart117 <lionheart117@foxmail.com>
2018-07-22 18:56:22 +0800

    update

--- tests/coinpair_prospector.py
+++ tests/coinpair_prospector.py
@@ -3,7 +3,7 @@
 from binance.BinanceAPI import *
 from restful.HuobiServices import *
 
-def CoinPairProspector(quoteCoin):
+def CoinPairProspector(baseCoinList, quoteCoinList):
 
     client = BinanceAPI('', '')
 
@@ -11,17 +11,37 @@ def CoinPairProspector(quoteCoin):
     HuobiCoinPairs = {}
     LegalCoinPairs = {}
 
-    for symbollist in client.get_exchange_info()['symbols']:
-        if symbollist['quoteAsset'] == quoteCoin:
-            BinanceCoinPairs[symbollist['symbol']] = True
+    if baseCoinList:
 
-    for symbollist in get_symbols()['data']:
-        if symbollist['quote-currency'] == quoteCoin.lower():
-            HuobiCoinPairs[symbollist['symbol'].upper()] = True
+        for symbollist in client.get_exchange_info()['symbols']:
+            for quoteCoin in quoteCoinList:
+                for baseCoin in baseCoinList:
+                    if symbollist['baseAsset'] == baseCoin and symbollist['quoteAsset'] == quoteCoin:
+                        BinanceCoinPairs[symbollist['symbol']] = True
 
-    for symbol in BinanceCoinPairs:
-        if symbol in HuobiCoinPairs:
-            LegalCoinPairs[symbol] = True
+        for symbollist in get_symbols()['data']:
+            for quoteCoin in quoteCoinList:
+                for baseCoin in baseCoinList:
+                    if symbollist['base-currency'] == baseCoin.lower() and symbollist['quote-currency'] == quoteCoin.lower():
+                        HuobiCoinPairs[symbollist['symbol'].upper()] = True
+
+        for symbol in BinanceCoinPairs:
+            if symbol in HuobiCoinPairs:
+                LegalCoinPairs[symbol] = True
+    else:
+        for symbollist in client.get_exchange_info()['symbols']:
+            for quoteCoin in quoteCoinList:
+                if symbollist['quoteAsset'] == quoteCoin:
+                    BinanceCoinPairs[symbollist['symbol']] = True
+
+        for symbollist in get_symbols()['data']:
+            for quoteCoin in quoteCoinList:
+                if symbollist['quote-currency'] == quoteCoin.lower():
+                    HuobiCoinPairs[symbollist['symbol'].upper()] = True
+
+        for symbol in BinanceCoinPairs:
+            if symbol in HuobiCoinPairs:
+                LegalCoinPairs[symbol] = True
 
     while(1):
         for symbol in LegalCoinPairs:
@@ -37,13 +57,11 @@ def CoinPairProspector(quoteCoin):
             ProfitH2B = (1 - 0.002 ) * huobiBidPrice   - (1 + 0.0005) * binanceAskPrice # Huobi  : sell Binance:buy
             ProfitB2H = (1 - 0.0005) * binanceBidPrice - (1 + 0.002 ) * huobiAskPrice   # Binance: sell Huobi  :buy
 
-            if ProfitH2B > 0:
-                print(symbol + ':' + ' H2B gain profit: %.8f' % ProfitH2B)
-            elif ProfitB2H > 0:
-                print(symbol + ':' + ' B2H gain profit: %.8f' % ProfitB2H)
+            if ProfitH2B > 0 or ProfitB2H > 0:
+                print(symbol + ':' + ' ONE  SIDE TRADE GAIN PROFIT: H2B:%.8f  B2H:%.8f\n' % (ProfitH2B, ProfitB2H))
             else:
-                print(symbol + ':' + ' BOTH SIDE TRADES DEFICIT: H2B:%.8f  B2H:%.8f' % (ProfitH2B, ProfitB2H))
+                print(symbol + ':' + ' BOTH SIDE TRADES DEFICIT: H2B:%.8f  B2H:%.8f\n' % (ProfitH2B, ProfitB2H))
 
 
 if __name__ == '__main__':
-    CoinPairProspector('ETH')
+    CoinPairProspector(['EOS','XRP','BCH','XLM','LTC','IOTA','ADA'],['ETH'])
